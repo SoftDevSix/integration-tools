9836d5b3bab4f67860e3ed4a194443fc2eabdfd2
Terán Rocha José Luis <joseluisteran2617@gmail.com>
2024-10-09 00:47:59 -0400

    chore: set up Java project using Gradle - Kotlin

---
+++ src/main/java/software/development/vi/integration/tools/IntegrationToolsApplication.java
@@ -0,0 +1,13 @@
+package software.development.vi.integration.tools;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntegrationToolsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntegrationToolsApplication.class, args);
+	}
+
+}
